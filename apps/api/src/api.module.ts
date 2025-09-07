33ed9460858f757544ebc9e6d90770dcb121c42a
abolfazl jazak <jazakabolfazl@gmail.com>
2025-09-08 01:55:48 +0330

    feat(database): Add DatabaseModule for PostgreSQL integration and update API module imports

--- apps/api/src/api.module.ts
+++ apps/api/src/api.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ApiController } from './api.controller';
 import { ApiService } from './api.service';
+import { DatabaseModule } from '@app/common/database';
 
 @Module({
-  imports: [],
+  imports: [
+    DatabaseModule
+  ],
   controllers: [ApiController],
   providers: [ApiService],
 })
---
+++ libs/common/src/database/database.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ConfigModule } from '../config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EntityClassOrSchema } from '@nestjs/typeorm/dist/interfaces/entity-class-or-schema.type';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT') || 5432,
+        database: configService.get('DB_NAME'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule {
+  static forFeature(entity: EntityClassOrSchema[]) {
+    return TypeOrmModule.forFeature(entity);
+  }
+}
