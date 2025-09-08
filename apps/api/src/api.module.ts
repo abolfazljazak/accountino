e5d1193292abb83aadd33200df9bb688ce59600e
abolfazl jazak <jazakabolfazl@gmail.com>
2025-09-08 17:03:20 +0330

    refactor(api): Remove ApiController and ApiService; introduce UsersModule with UsersController and UsersService

--- apps/api/src/api.module.ts
+++ apps/api/src/api.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ApiController } from './api.controller';
-import { ApiService } from './api.service';
 import { DatabaseModule } from '@app/common/database';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [
-    DatabaseModule
-  ],
-  controllers: [ApiController],
-  providers: [ApiService],
+  imports: [DatabaseModule, UsersModule],
+  controllers: [],
+  providers: [],
 })
 export class ApiModule {}
