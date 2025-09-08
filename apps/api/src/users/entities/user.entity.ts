5f14393ab8821d8004e4a1d014611507b6911443
abolfazl jazak <jazakabolfazl@gmail.com>
2025-09-08 18:28:10 +0330

    feat(api): Add UserEntity, Roles, and UserStatus enums for user management

---
+++ apps/api/src/users/entities/user.entity.ts
@@ -0,0 +1,37 @@
+import { Column } from 'typeorm';
+import { BaseEntity, Entity } from 'typeorm';
+import { UserStatus } from '../enums/status.enum';
+import { Roles } from '../enums/role.enum';
+
+@Entity('User')
+export class UserEntity extends BaseEntity {
+  @Column({ nullable: true })
+  first_name: string;
+
+  @Column({ nullable: true })
+  last_name: string;
+
+  @Column({ default: Roles.User })
+  role: string;
+
+  @Column({ default: UserStatus.Active })
+  status: string;
+
+  @Column({ unique: true, nullable: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ unique: true, nullable: true })
+  phone: string;
+
+  @Column()
+  wallet_balance: number;
+
+  @Column()
+  created_at: Date;
+
+  @Column()
+  updated_at: Date;
+}
