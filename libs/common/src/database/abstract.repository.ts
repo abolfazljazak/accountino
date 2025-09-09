9d7b63918a71ad6a25bc7bcb5d6b9f49fc0816db
abolfazl jazak <jazakabolfazl@gmail.com>
2025-09-09 12:39:47 +0330

    feat(database): Implement AbstractRepository class for common database operations

---
+++ libs/common/src/database/abstract.repository.ts
@@ -0,0 +1,49 @@
+import {
+  DeepPartial,
+  DeleteResult,
+  FindOneOptions,
+  FindOptionsWhere,
+  Repository,
+} from 'typeorm';
+import { QueryDeepPartialEntity } from 'typeorm/query-builder/QueryPartialEntity';
+import { NotFoundException } from '@nestjs/common';
+import { AbstractEntity } from '@app/common';
+
+export abstract class AbstractRepository<TEntity extends AbstractEntity> {
+  constructor(protected readonly repository: Repository<TEntity>) {}
+
+  async create(document: Omit<TEntity, 'id'>): Promise<TEntity> {
+    const newDocument = this.repository.create(
+      document as DeepPartial<TEntity>,
+    );
+    return await this.repository.save(newDocument);
+  }
+
+  async findOneById(id: FindOptionsWhere<TEntity>): Promise<TEntity> {
+    const document = await this.repository.findOneBy(id);
+    if (!document) throw new NotFoundException('Document was not found');
+    return document;
+  }
+
+  async find(): Promise<TEntity[]> {
+    return this.repository.find();
+  }
+
+  async update(
+    id: FindOptionsWhere<TEntity>,
+    document: QueryDeepPartialEntity<TEntity>,
+  ): Promise<TEntity> {
+    const updateDocument = await this.repository.update(id, document);
+    if (!updateDocument) throw new NotFoundException('Document was not found');
+    return this.findOneById(id);
+  }
+
+  async delete(id: FindOptionsWhere<TEntity>): Promise<DeleteResult> {
+    const document = await this.repository.delete(id);
+    if (!document) {
+      throw new NotFoundException('Document was not found');
+    }
+
+    return document;
+  }
+}
